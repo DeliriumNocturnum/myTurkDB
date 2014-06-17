68c20654e77f3dfc466916a9d27ddab7580f5e8d
DeliriumNocturnum <jake@schwartzpub.com>
2014-06-17 14:27:00 -0500

    removed invalid flag from windows bootstrap

--- Quick_Setup/bootstrap_windows.bat
+++ Quick_Setup/bootstrap_windows.bat
@@ -1,5 +1,5 @@
 @echo Downloading Dependencies (.Net 4 for Windows)
-.\wget\wget --no-check-certificate http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe
+.\wget\wget http://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe
 
 @echo Downloading Dependencies (Pip for Windows)
 
